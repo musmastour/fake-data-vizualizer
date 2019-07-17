68f4ba8d4229bbbfd313d195683a0fc236c8cbd5
Mus <mus@MacBook-Pro-de-Mus.local>
2019-07-17 14:42:56 +0300

    add get all engines

--- index.js
+++ index.js
@@ -15,8 +15,26 @@ const settings = {
     port: process.env.PORT || 3000
 }
 
+const pool = new pg.Pool({
+    user: 'wiyodttjlbynyw',
+    host: 'ec2-54-247-170-5.eu-west-1.compute.amazonaws.com',
+    database: 'd6doqnveou4tn9',
+    password: '7d1c7aba578249c12407ffa3e93a81960dd6f6c611eb793b3b26bedf204122c1',
+    port: '5432'
+});
+
 app.get('/test', (req, res) => {
     res.send('MUSTAPHA MASTOUR');
+});
+
+app.get('/get/all/engines', (req, res) => {
+    pool.query('SELECT * FORM engine', (err, result) => {
+        if (err && result === undefined) {
+            console.log(err)
+            throw err
+        }
+        res.send(result.rows);
+    })
 });
 
 app.listen(settings.port, function() {
